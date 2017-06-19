af9251ab381aa96e54b598f2f927f8818a39eff1
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2017-06-19 20:47:20 -0300

    refactoring

--- asteroidea/complete_learner.py
+++ asteroidea/complete_learner.py
@@ -17,7 +17,7 @@ class Learner(object):
         self.configs_tables = parser.build_configs_tables(self.model)
 
             
-    def learn_parameters(self, dataset):
+    def learn_parameters(self, dataset, relational_data=False):
         """Find the (exact or approximated) optimal parameters for the dataset.
         Before running this function, make sure you have read a structure file
         compatible with the variables present in the dataset.
@@ -26,24 +26,22 @@ class Learner(object):
         epsilon -- stopping criteria
         Missing values in the dataset should be represented as NaN.
         """
-        model = self.model
-        configs_tables = self.configs_tables
-        # ensure dataset is a pandas dataframe
-        if not isinstance(dataset, pd.DataFrame):
-            raise TypeError('dataset should be a Pandas DataFrame')
-        # ensure all variables in the dataset are head of a rule
-        for column in dataset.columns:
-            if not column in model:
-                # what to do if column in dataset is not head of any rule?
-                raise Exception('Column %s in dataset is not head of any rule.'
-                                % column)
+        self.dataset = dataset
+        self._verify_dataset()
+        if relational_data:
+            self._update_count_relational()
+        else:
+            self._update_count_propositional()
+        return self._find_optimal_parameters()
 
+    
+    def _update_count_propositional(self):
         # count the number of occurences of each configuration for each family
-        for i, row in dataset.iterrows():
-            for head in configs_tables:
-                configs_table = configs_tables[head]
+        for i, row in self.dataset.iterrows():
+            for head in self.configs_tables:
+                configs_table = self.configs_tables[head]
                 config_vars = {head: row[head]}
-                for parent in model[head]['parents']:
+                for parent in self.model[head]['parents']:
                     config_vars[parent] = row[parent]
                 # get the index of the configs_table that corresponds to the
                 # configuration of the current example
@@ -54,14 +52,19 @@ class Learner(object):
                 index = df.index.values[0]
                 # updates the count in configs_table
                 configs_table.loc[index, 'count'] += 1
-        
 
+
+    def _update_count_relational(self, dataset):
+        print('')
+
+
+    def _find_optimal_parameters(self):
         # maximize the likelihood by finding the optimal parameters
         ll = 0
         new_params = {}
-        for head in model:
-            rules = model[head]['rules']
-            configs_table = configs_tables[head]
+        for head in self.model:
+            rules = self.model[head]['rules']
+            configs_table = self.configs_tables[head]
             optimal_params = calculations.exact_optimization(head, configs_table)
             if optimal_params == False:
                 self.info['no_exact_solution'].add(head)
@@ -72,18 +75,28 @@ class Learner(object):
                 res = minimize(
                         calculations.head_log_likelihood,
                         initial_guess,
-                        args = (head, model, configs_table, -1.0),
+                        args = (head, self.model, configs_table, -1.0),
                         method = 'L-BFGS-B',
                         bounds = [(0.0, 1.0)]*len(initial_guess),
                         # bounds = [(0.001,0.999)]*len(initial_guess),
                         options = {'disp': True ,'eps' : 1e-7})
                 optimal_params = res.x.tolist()
-
             # update log-likelihood
-            ll += calculations.head_log_likelihood(optimal_params, head, model,
-                                                   configs_table)
+            ll += calculations.head_log_likelihood(optimal_params, head,
+                                                   self.model, configs_table)
             # store new parameters
             new_params[head] = optimal_params
-        
         return {'log-likelihood': ll,
                 'optimal parameters': new_params}
+
+
+    def _verify_dataset(self):
+        # ensure dataset is a pandas dataframe
+        if not isinstance(self.dataset, pd.DataFrame):
+            raise TypeError('dataset should be a Pandas DataFrame')
+        # ensure all variables in the dataset are head of a rule
+        for column in self.dataset.columns:
+            if not column in self.model:
+                # what to do if column in dataset is not head of any rule?
+                raise Exception('Column %s in dataset is not head of any rule.'
+                                % column)
