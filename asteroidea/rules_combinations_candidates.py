dd7aae082b6cfb1764ec9748a46e6b1fbaba1198
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2017-06-23 21:46:29 -0300

    added structure learning files

---
+++ asteroidea/rules_combinations_candidates.py
@@ -0,0 +1,198 @@
+Variable: x0
+    Parents: 
+        Combination 1
+            x0:-
+    Parents: x1
+        Combination 1
+            x0:-x1
+        Combination 2
+            x0:-/+x1
+        Combination 3
+            x0:-x1
+            x0:-/+x1
+    Parents: x1,x2
+        Combination 1
+            x0:-x1,x2
+        Combination 2
+            x0:-x1,/+x2
+        Combination 3
+            x0:-/+x1,x2
+        Combination 4
+            x0:-/+x1,/+x2
+        Combination 5
+            x0:-
+            x0:-x1,x2
+        Combination 6
+            x0:-
+            x0:-x1,/+x2
+        Combination 7
+            x0:-
+            x0:-/+x1,x2
+        Combination 8
+            x0:-
+            x0:-/+x1,/+x2
+            
+        Combination 9
+            x0:-x1
+            x0:-x2
+        Combination 11
+            x0:-x1
+            x0:-/+x2
+        Combination 16
+            x0:-/+x1
+            x0:-x2
+        Combination 18
+            x0:-/+x1
+            x0:-/+x2
+            
+        Combination 14
+            x0:-x1
+            x0:-/+x1,x2
+        Combination 15
+            x0:-x1
+            x0:-/+x1,/+x2
+        Combination 20
+            x0:-/+x1
+            x0:-x1,x2
+        Combination 21
+            x0:-/+x1
+            x0:-x1,/+x2
+        Combination 24
+            x0:-x2
+            x0:-x1,/+x2
+        Combination 25
+            x0:-x2
+            x0:-/+x1,/+x2
+        Combination 26
+            x0:-/+x2
+            x0:-x1,x2
+        Combination 28
+            x0:-/+x2
+            x0:-/+x1,x2
+
+        Combination 13
+            x0:-x1,x2
+            x0:-x1,/+x2
+        Combination 23
+            x0:-x1,x2
+            x0:-/+x1,x2
+        Combination 29
+            x0:-x1,x2
+            x0:-/+x1,/+x2
+        Combination 27
+            x0:-x1,/+x2
+            x0:-/+x1,/+x2
+        Combination 30
+            x0:-x1,/+x2
+            x0:-/+x1,x2
+        Combination 22
+            x0:-/+x1,x2
+            x0:-/+x1,/+x2
+
+
+
+
+            
+        
+        Combination 31
+            x0:-
+            x0:-x1
+            x0:-x2
+        Combination 32
+            x0:-
+            x0:-x1
+            x0:-/+x2
+        Combination 33
+            x0:-
+            x0:-/+x1
+            x0:-x2
+        Combination 34
+            x0:-
+            x0:-/+x1
+            x0:-/+x2
+        Combination 48
+            x0:-
+            x0:-x1,x2
+            x0:-/+x1,/+x2
+        Combination 49
+            x0:-
+            x0:-x1,/+x2
+            x0:-/+x1,x2
+
+            ----------
+            
+        Combination 35
+            x0:-x1
+            x0:-/+x1
+            x0:-x2
+        Combination 36
+            x0:-x1
+            x0:-/+x1
+            x0:-/+x2
+        Combination 37
+            x0:-x1
+            x0:-x2
+            x0:-/+x2
+        Combination 40
+            x0:-/+x1
+            x0:-x2
+            x0:-/+x2
+        
+        Combination 38
+            x0:-x1
+            x0:-x2
+            x0:-/+x1,/+x2
+        Combination 39
+            x0:-x1
+            x0:-/+x2
+            x0:-/+x1,x2
+        Combination 41
+            x0:-/+x1
+            x0:-x2
+            x0:-x1,/+x2
+        Combination 42
+            x0:-/+x1
+            x0:-/+x2
+            x0:-x1,x2
+        
+        Combination 44
+            x0:-/+x1
+            x0:-x1,x2
+            x0:-x1,/+x2
+        Combination 47
+            x0:-x2
+            x0:-x1,/+x2
+            x0:-/+x1,/+x2
+        Combination 46
+            x0:-/+x2
+            x0:-x1,x2
+            x0:-/+x1,x2
+        Combination 45
+            x0:-x1
+            x0:-/+x1,x2
+            x0:-/+x1,/+x2
+
+            ----------------------
+        
+        Combination 10
+            x0:-x1,x2
+            x0:-x1,/+x2
+            x0:-/+x1,x2
+        Combination 17
+            x0:-x1,x2
+            x0:-/+x1,x2
+            x0:-/+x1,/+x2
+        Combination 12
+            x0:-x1,x2
+            x0:-x1,/+x2
+            x0:-/+x1,/+x2
+        Combination 19
+            x0:-x1,/+x2
+            x0:-/+x1,x2
+            x0:-/+x1,/+x2
+            
+        Combination 43
+            x0:-x1,x2
+            x0:-x1,/+x2
+            x0:-/+x1,x2
+            x0:-/+x1,/+x2
