1912618d5d24d56474561738565ba2c90d3fde2a
franciscodefaria <franciscohovfaria@gmail.com>
2017-07-31 15:37:20 -0300

    fix: propositional not working after smart configuration queries

--- asteroidea/missing_learner.py
+++ asteroidea/missing_learner.py
@@ -32,6 +32,9 @@ class Learner(object):
         self.model = parser.read_structure(structure_filepath, relational_data=relational_data)
         self.configs_tables = parser.build_configs_tables(self.model)
 
+        if not relational_data:
+            self._read_propositional_dataset()
+
         for consistency_test in[True, False]:
             self.problog_model_str=parser.build_problog_model_str(
                                     self.model, self.configs_tables,
@@ -47,11 +50,15 @@ class Learner(object):
             else:
                 self.knowledge = Inference(self.problog_model_str,
                                            probabilistic_data=probabilistic_data,
-                                           relational_data=True)
+                                           relational_data=relational_data)
                 model=self.model
                 self.knowledge.update_weights(model)
                 try:
-                    self.knowledge.eval()
+                    if not self.relational_data:
+                        for i, row in self.propositional_dataset.iterrows():
+                            res = self.knowledge.eval(evidence=row)
+                    else:
+                        self.knowledge.eval()
                 except:
                     raise InconsistentEvidenceError("""This error may have occured
                         because some observation in the dataset is impossible given
@@ -70,8 +77,6 @@ class Learner(object):
         self._log_time('Others')
         model = self.model
         configs_tables = self.configs_tables
-        if not self.relational_data:
-            self._read_propositional_dataset()
 
         # begin EM cycle
         old_ll = None
@@ -95,11 +100,12 @@ class Learner(object):
                     for head in configs_tables:
                         configs_table = configs_tables[head]
                         for c, config in configs_table.iterrows():
-                            update_in_count = res[config['dumb_var']]
-                            configs_table.loc[c, 'count'] += update_in_count
-                            if step==0:
-                                if update_in_count==1:
-                                    configs_table.loc[c, 'real_count'] += update_in_count
+                            if config['dumb_var'] in res:
+                                update_in_count = res[config['dumb_var']]
+                                configs_table.loc[c, 'count'] += update_in_count
+                                if step==0:
+                                    if update_in_count==1:
+                                        configs_table.loc[c, 'real_count'] += update_in_count
             else:
                 res = self.knowledge.eval()
                 for head in configs_tables:
