ebbd36e165960f92a81ec7e98c31b8521d78f7b0
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2018-02-25 17:17:04 -0300

    testing: standardized generate datasets file

--- experiments/generate_datasets.py
+++ experiments/generate_datasets.py
@@ -8,24 +8,41 @@ from tools.build_propositional_datasets import create_propositional_datasets
 # from build_relational_datasets import create_relational_datasets
 
 
-# PROPOSITIONAL DATASETS
-
 # We are going to create 8 instances of each dataset. In order to do that, we'll copy the original folders to 8 different instances and run the function for each one. This ensures that the datasets will be different, due to the pseudo randomicity of the sample function.
 
-dataset_paths = []
-for i in range(1, 9):
-    path = "./datasets/ship_energy_plant_{:02d}".format(i)
-    if os.path.exists(path):
-        raise RuntimeError('Path already exists: {}'.format(path))
-    dataset_paths.append(path)
+def create_all_dataset_instances(dataset_name, relational, size_range, missing_range):
+    dataset_paths = []
+    for i in range(1, 9):
+        path = "./datasets/{}_{:02d}".format(dataset_name, i)
+        if os.path.exists(path):
+            raise RuntimeError('Path already exists: {}'.format(path))
+        dataset_paths.append(path)
 
-tarf = tarfile.open('./datasets/ship_energy_plant.tar.gz')
-for path in dataset_paths:
-    tarf.extractall(path=path)
-tarf.close()
+    tarf = tarfile.open('./datasets/{}.tar.gz'.format(dataset_name))
+    for path in dataset_paths:
+        tarf.extractall(path=path)
+    tarf.close()
 
-size_range = [2,4]
-missing_range = [0, 10]
+    for path in dataset_paths:
+        if not relational:
+            create_propositional_datasets(path, size_range, missing_range)
+        else:
+            create_relational_datasets(path, size_range, missing_range)
 
-for path in dataset_paths:
-    create_propositional_datasets(path, size_range, missing_range)
+
+# PROPOSITIONAL DATASETS
+
+create_all_dataset_instances(
+    dataset_name='ship_energy_plant',
+    relational=False,
+    size_range=[2,4],
+    missing_range=[0, 10])
+
+
+# RELATIONAL DATASETS
+
+create_all_dataset_instances(
+    dataset_name='alarm',
+    relational=True,
+    size_range=[1,2],
+    missing_range=[0, 10])
